a2dec40e0e7be492afe0c2c93fa9b321c0db0c4b
Rindra <hei.rindra.2@gmail.com>
2025-08-15 22:04:47 +0300

    feat(router): add GET /:id route for fetching a single character and reorder route :id

--- backend/src/routes/characterRoutes.ts
+++ backend/src/routes/characterRoutes.ts
@@ -1,11 +1,13 @@
 import {Router} from "express";
-import {getCharacters,addCharacter,updateCharacter,deleteCharacter} from "../controllers/characterController";
+import {getCharacters,getCharacterById,addCharacter,updateCharacter,deleteCharacter} from "../controllers/characterController";
 
 const router = Router();
 
 router.get('/', getCharacters);
 router.post('/',addCharacter);
-router.put('/:id',updateCharacter);
-router.delete('/:id', deleteCharacter)
+router.route('/:id')
+    .get(getCharacterById)
+    .put(updateCharacter)
+    .delete(deleteCharacter)
 
 export default router;
