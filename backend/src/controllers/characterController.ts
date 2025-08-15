07c34cee92ad52888bab36085deb329ee436338c
Rindra <hei.rindra.2@gmail.com>
2025-08-15 21:24:05 +0300

    feat(controller): add updateCharacter function and generate new ID in addCharacter

--- backend/src/controllers/characterController.ts
+++ backend/src/controllers/characterController.ts
@@ -9,9 +9,22 @@ export async function getCharacters(req: Request,res: Response) {
 
 export async function addCharacter(req: Request, res: Response) {
     const characters = await readCharacters();
-    const newCharacter = {...req.body}
+    const newId = characters.length ? characters[characters.length - 1].id + 1 : 1;
+    const newCharacter = { id: newId, ...req.body };
     characters.push(newCharacter);
     await writeCharacters(characters)
     res.status(200).json(newCharacter);
 }
 
+export async function updateCharacter(req: Request, res: Response) {
+    const {id} = req.params;
+    const characters = await readCharacters();
+    const index = characters.findIndex((c: { id: number; }) => c.id === parseInt(id));
+
+    if(index === -1){
+        return res.status(404).json({message: "Character not found."});
+    }
+    characters[index]={...characters[index], ...req.body};
+    await writeCharacters(characters);
+    res.json(characters[index])
+}
