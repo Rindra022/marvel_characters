1aa8142ac1bbb84292f622a22651e72fa575579b
Rindra <hei.rindra.2@gmail.com>
2025-08-15 18:17:24 +0300

    feat: add new character

--- backend/src/controllers/characterController.ts
+++ backend/src/controllers/characterController.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
-import {readCharacters} from "../models/characterModel";
+import {readCharacters, writeCharacters} from "../models/characterModel";
 
 
 export async function getCharacters(req: Request,res: Response) {
     const characters = await readCharacters();
     res.json(characters);
+}
+
+export async function addCharacter(req: Request, res: Response) {
+    const characters = await readCharacters();
+    const newCharacter = {...req.body}
+    characters.push(newCharacter);
+    await writeCharacters(characters)
+    res.status(200).json(newCharacter);
 }
 
