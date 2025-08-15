b0d59b8cd65a6b09a591955a1753aaadf7047920
Rindra <hei.rindra.2@gmail.com>
2025-08-15 21:22:22 +0300

    fix(controller): ensure readCharacters returns array and writeCharacters handles array format

--- backend/src/models/characterModel.ts
+++ backend/src/models/characterModel.ts
@@ -5,9 +5,10 @@ const filePath = path.resolve(__dirname,"../data/characters.json")
 
 export async function readCharacters(){
     const data = await fs.readFile(filePath, "utf8")
-    return JSON.parse(data)
+    const parsed= JSON.parse(data)
+    return parsed.characters;
 }
 
 export async function writeCharacters(characters: any[]){
-    await  fs.writeFile(filePath, JSON.stringify(characters))
+    await  fs.writeFile(filePath, JSON.stringify({characters},null , 2))
 }
