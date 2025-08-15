0618b8e6274da01ad699e27d8c4a7448bbb072ff
Rindra <hei.rindra.2@gmail.com>
2025-08-15 21:34:05 +0300

    feat(router): add DELETE route for characters

--- backend/src/routes/characterRoutes.ts
+++ backend/src/routes/characterRoutes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import {getCharacters,addCharacter,updateCharacter} from "../controllers/characterController";
+import {getCharacters,addCharacter,updateCharacter,deleteCharacter} from "../controllers/characterController";
 
 const router = Router();
 
 router.get('/', getCharacters);
 router.post('/',addCharacter);
 router.put('/:id',updateCharacter);
+router.delete('/:id', deleteCharacter)
 
 export default router;
