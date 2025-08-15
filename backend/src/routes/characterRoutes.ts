6681d40b62b4df581b4344e11acaa6be9d66b3fd
Rindra <hei.rindra.2@gmail.com>
2025-08-15 21:25:02 +0300

    feat(router): add POST and PUT routes for characters

--- backend/src/routes/characterRoutes.ts
+++ backend/src/routes/characterRoutes.ts
@@ -1,8 +1,10 @@
 import {Router} from "express";
-import {getCharacters} from "../controllers/characterController";
+import {getCharacters,addCharacter,updateCharacter} from "../controllers/characterController";
 
 const router = Router();
 
 router.get('/', getCharacters);
+router.post('/',addCharacter);
+router.put('/:id',updateCharacter);
 
 export default router;
